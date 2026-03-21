3b19406698eaf0fb24777dd6d582f3720cebe1bd
zhenia-chugaev <zhenia.chugaev@bk.ru>
2026-03-21 14:02:45 +0100

    Test user interaction

--- __tests__/main.spec.jsx
+++ __tests__/main.spec.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Widget from '@hexlet/chatbot-v2'
 import steps from '../__fixtures__/steps.json'
@@ -7,4 +8,28 @@ test('widget initialization', () => {
   render(Widget(steps))
   const chatButton = screen.getByRole('button', { name: 'Открыть Чат' })
   expect(chatButton).toBeInTheDocument()
+})
+
+it('opens and closes chatbot dialog', async () => {
+  const user = userEvent.setup()
+  render(Widget(steps))
+  await user.click(screen.getByRole('button', { name: 'Открыть Чат' }))
+  expect(screen.getByText('Виртуальный помощник')).toBeInTheDocument()
+  await user.click(screen.getByRole('button', { name: 'Close' }))
+  expect(screen.queryByText('Виртуальный помощник')).not.toBeInTheDocument()
+})
+
+test('moving through the chatbot steps', async () => {
+  const user = userEvent.setup()
+  render(Widget(steps))
+  await user.click(screen.getByRole('button', { name: 'Открыть Чат' }))
+  expect(screen.getByText('Начало')).toBeInTheDocument()
+  await user.click(screen.getByRole('button', { name: 'Следующий шаг' }))
+  expect(screen.getByText('Куда вы хотите перейти?')).toBeInTheDocument()
+  await user.click(screen.getByRole('button', { name: 'Следующий шаг' }))
+  expect(screen.getByText('Конец')).toBeInTheDocument()
+  expect(screen.getByText('Выберите вариант!')).toBeInTheDocument()
+  await user.click(screen.getByRole('button', { name: 'Предыдущий шаг' }))
+  await user.click(screen.getByRole('button', { name: 'В начало' }))
+  expect(screen.getAllByText('Начало')).toHaveLength(2)
 })
--- vitest.setup.js
+++ vitest.setup.js
@@ -1,2 +1,5 @@
+import { vi } from 'vitest'
 import '@testing-library/jest-dom/vitest'
 import '@hexlet/chatbot-v2/styles'
+
+window.HTMLElement.prototype.scrollIntoView = vi.fn()
