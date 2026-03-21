205f2eba76c1ee999634084083615fd5d4d6c15b
zhenia-chugaev <zhenia.chugaev@bk.ru>
2026-03-21 14:01:40 +0100

    Set up Vitest Preview

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    css: true,
     setupFiles: ['./vitest.setup.js'],
     server: {
       deps: {
--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,2 @@
 import '@testing-library/jest-dom/vitest'
+import '@hexlet/chatbot-v2/styles'
